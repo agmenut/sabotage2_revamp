0d4c39dff5e0e03d77f04c0e917c73e0c6beadec
arnaud <arnaud@shiva.omsoft.com>
2014-08-14 19:34:38 -0700

    Initial Commit

---
+++ s2.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, redirect, request, send_from_directory
+from werkzeug.contrib.fixers import ProxyFix
+from datetime import date, timedelta
+
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the 
+    front-end server to add these headers, to let you quietly bind 
+    this to a URL other than / and to an HTTP scheme that is 
+    different than what is used locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+
+app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
+
+@app.route('/')
+def start():
+	return render_template("index.html")
+
+@app.route('/s2/oldnews/', defaults={'page': 'oldnews'})
+@app.route('/s2/page=<page>/')
+def other_page(page):
+	if (page == "oldnews"):
+		return redirect('http://www.sabotage2.com/index.php?page=oldnews', 301)
+	else:
+		return 'Page does not exist', 404
+
+@app.route('/s2/robots.txt')
+def robots():
+	return send_from_directory(app.static_folder, request.path[1:])
+	
+@app.errorhandler(404)
+def page_not_found(e):
+	return '404 Is full of goats', 404
+
+if __name__ == '__main__':
+	app.run()
