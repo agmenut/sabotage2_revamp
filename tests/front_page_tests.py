950135cd2c88e76eb9fefce1c7e5b4138939f966
Arnaud Menut <agmenut@gmail.com>
2014-08-16 17:45:00 -0700

    More unit tests for db connectivity.

--- tests/front_page_tests.py
+++ tests/front_page_tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from TestBase import TestBase
-from haxorbb import app
+from haxorbb import app, db
+from haxorbb.models import Articles
 
 
 class FrontPageTests(TestBase):
@@ -8,3 +9,8 @@ class FrontPageTests(TestBase):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+
+    def test_database_connection(self):
+        #tester = app.test_client(self)
+        count = Articles.query.count()
+        self.assertIsNotNone(count)
