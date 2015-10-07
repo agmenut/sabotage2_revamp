262bd6b051236052eebb1ee5d2680ac2e3b49ed5
Arnaud Menut <agmenut@gmail.com>
2015-10-07 07:49:54 -0700

    More auth handing routes. Moving rest of images out of app structure.

--- haxorbb/auth/views.py
+++ haxorbb/auth/views.py
@@ -3,6 +3,18 @@ from flask import render_template
 from . import auth
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('auth/login.html')
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('auth/register.html')
+
+
+@auth.route('/logout', methods=['POST'])
+def logout():
+    pass
+
+
