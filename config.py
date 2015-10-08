43fb005347cfc0936d055e430b2ad259279b55d1
Arnaud Menut <agmenut@gmail.com>
2015-10-08 06:57:08 -0700

    Pruned gitignore. Updated templates for media url handling. More progress on config getting host data from .env file.

--- config.py
+++ config.py
@@ -1,22 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
-import json
-
-_cwd = os.path.dirname(os.path.abspath(__file__))
-
-
-# with open(os.path.join(_cwd, 'local.conf')) as config_file:
-#     db_conf = json.load(config_file)
-#
-# DB_USER = db_conf['haxorbb']['user']
-# DB_PASS = db_conf['haxorbb']['pass']
-# DB_HOST = db_conf['haxorbb']['host']
-# MEDIA = "<PATH TO MEDIA DIR>"
 
 
 class BaseConfiguration(object):
     DEBUG = False
-    MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
+    MEDIA_ROOT = os.environ.get('MEDIA')
 
     @staticmethod
     def initapp(app):
@@ -27,7 +15,6 @@ class DevelopmentConfig(BaseConfiguration):
     DEBUG = True
     TESTING = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    print "Database url:", os.environ.get('DATABASE_URL')
 
     @classmethod
     def initapp(cls, app):
