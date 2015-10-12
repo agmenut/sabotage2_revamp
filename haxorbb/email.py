393e6368742a099b36b25a6a6004c6003c9fcdd9
agmenut@gmail.com <agmenut@gmail.com>
2015-10-12 10:38:31 -0700

    Starting the thread will actually make it run

--- haxorbb/email.py
+++ haxorbb/email.py
@@ -6,13 +6,13 @@ from . import mail
 
 def send_async_email(app, msg):
     with app.app_context():
-        mail.send(msg)
-
+            mail.send(msg)
 
 def send_mail(to, subject, template, **kwargs):
-    app = current_app.app_context()
+    app = current_app._get_current_object()
     msg = Message(app.config['MAIL_PREFIX'] + ' ' + subject, sender=app.config['MAIL_REPLY'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
     return thr
