9bfcc284f9b65de1e63bf6e0e58ea800daa76240
agmenut@gmail.com <agmenut@gmail.com>
2015-10-11 20:31:24 -0700

    Move imports after the env file is read

--- s2.py
+++ s2.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 import os
-from haxorbb import initialize_app, db
-from flask.ext.script import Manager
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -10,10 +8,15 @@ if os.path.exists('.env'):
     with open('.env') as env:
         for line in env:
             var = line.strip().split('=')
+            print var
             if len(var) == 2:
                 os.environ[var[0]] = var[1]
 
+from haxorbb import initialize_app, db
+from flask.ext.script import Manager
+
 app = initialize_app(os.getenv('FLASK_CONFIG'))
+print app.config
 manager = Manager(app)
 
 if __name__ == '__main__':
