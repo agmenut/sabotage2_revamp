478624a271c0ba43679ddd62bbcef0bbd7c59983
Arnaud Menut <agmenut@gmail.com>
2015-11-02 07:19:12 -0800

    Start of article creation and editing.

---
+++ haxorbb/front_page/forms.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+from flask.ext.wtf import Form
+from flask import Markup
+from wtforms import (Field, StringField, BooleanField, TextAreaField)
+from wtforms.validators import Length, DataRequired
+from wtforms.widgets.core import html_params
+
+
+class ButtonWidget(object):
+    html_params = staticmethod(html_params)
+
+    def __init__(self, input_type='submit', text=''):
+        self.input_type = input_type
+        self.text = text
+
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault('id', field.id)
+        kwargs.setdefault('type', field.type)
+        if 'value' not in kwargs:
+            kwargs['value'] = field._value()
+        return Markup('<button type="submit"{}>{}</button>'.format(
+            self.html_params(name=field.name, **kwargs), field.text)
+        )
+
+
+class Button(Field):
+    widget = ButtonWidget()
+
+    def __init__(self, label=None, validators=None, text="Submit", **kwargs):
+        super(Button, self).__init__(label, validators, **kwargs)
+        self.text = text
+
+    def _value(self):
+        if self.data:
+            return u''.join(self.data)
+        else:
+            return u''
+
+
+class Compose(Form):
+    title = StringField('Title', validators=[DataRequired(), Length(1, 255)])
+    body = TextAreaField('Article', validators=[DataRequired(), Length(min=1)])
+    visibility = BooleanField('Publicly Visible')
+    submit = Button('Submit')
+
+
+class Edit(Form):
+    title = StringField('Title', validators=[DataRequired(), Length(1, 255)])
+    body = TextAreaField('Article', validators=[DataRequired(), Length(min=1)])
+    visibility = BooleanField('Publicly Visible')
+    submit = Button('Submit')
--- haxorbb/front_page/views.py
+++ haxorbb/front_page/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from . import front_page
+from .forms import Edit, Compose
 from ..models import Articles
-from flask import render_template, redirect
-from flask.ext.login import current_user
-
+from flask import render_template, redirect, g
+from flask.ext.login import current_user, login_required
+import bleach
 
 @front_page.before_app_request
 def before_request():
@@ -19,6 +20,22 @@ def home_page(article):
     else:
         articles = [article for article in Articles.query.order_by(Articles.datestamp.desc()).all()]
     return render_template("front_page/index.html", articles=articles)
+
+
+@front_page.route('/article/<articleid>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_article(articleid):
+    article = Articles.query.filter_by(id=articleid).first()
+    form = Edit()
+    if form.validate_on_submit():
+        print "Update article {}".format(articleid)
+        print form.body.data
+        print bleach.linkify(form.body.data)
+    g.articleid = articleid
+    form.title.data = article.title
+    form.body.data = article.content
+    form.visibility.data = article.visibility
+    return render_template("front_page/edit.html", user=current_user, form=form)
 
 
 @front_page.route('/oldnews/', defaults={'page': 'oldnews'})
