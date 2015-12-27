1fa0e7690621e951c48461b46c9e4ab48c82035a
Arnaud Menut <agmenut@gmail.com>
2015-12-27 12:09:30 -0800

    Moved image scaler out of filter module into utilities module.

--- haxorbb/utilities/filters.py
+++ haxorbb/utilities/filters.py
@@ -2,10 +2,6 @@
 from flask import Blueprint, current_app
 from flask_login import current_user
 import pytz
-from PIL import Image
-from StringIO import StringIO
-import os.path
-
 
 filters = Blueprint('filters', __name__)
 
@@ -31,22 +27,3 @@ filters.add_app_template_filter(datetime_filter)
 def forum_datetime(value):
     return base_datetime_filter(value, '%b %d, %Y %H:%M')
 filters.add_app_template_filter(forum_datetime)
-
-
-def create_timg(img):
-    img = os.path.split(img)[1]
-    path = os.path.join(current_app.config['MEDIA_ROOT'], 'users', current_user.username, img)
-    image_buffer = StringIO()
-    im = Image.open(path)
-    format_ = im.format
-    target_w = 300  # TODO: Replace MAGIC NUMBER with parameter
-    ratio = float(im.size[0]) / float(im.size[1])
-    target_h = int(target_w / ratio)
-    resized = im.resize((target_w, target_h), Image.ANTIALIAS)
-    resized.save(image_buffer, format_)
-    image_buffer.seek(0)
-    target_file = os.path.join(current_app.config['MEDIA_ROOT'], 'users', 'tn', 'tn_{}'.format(img))
-    if not os.path.exists(os.path.join(current_app.config['MEDIA_ROOT'], 'users', 'tn')):
-        os.mkdir(os.path.join(current_app.config['MEDIA_ROOT'], 'users', 'tn'))
-    with open(target_file, 'wb') as imgfile:
-        imgfile.write(image_buffer.read())
--- haxorbb/utilities/utils.py
+++ haxorbb/utilities/utils.py
@@ -1,5 +1,10 @@
 # *-* coding: utf-8 *-*
 import string
+import os
+from io import BytesIO
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+from warnings import warn
 
 
 class Utilities(object):
@@ -13,3 +18,26 @@ class Utilities(object):
             word = word.translate(symbol_map)
             slug.append(word)
         return delimiter.join(slug)[:30]
+
+
+def generate_thumbnail(source, dest_path, source_path=None, height=None, width=None, prefix='tn_', preserve_exif=False):
+    if height is None and width is None:
+        raise ValueError('Either height or width value required')
+    if height and width:
+        warn("Both height and width are set, defaulting to width", UserWarning)
+    path, img = os.path.split(source)
+    if source_path:
+        path = source_path
+    image_buffer = BytesIO()
+    im = Image.open(os.path.join(path, source))
+    ratio = float(im.size[0]) / float(im.size[1])
+    if width:
+        height = int(width / ratio)
+    elif width is None and height:
+        width = int(height / ratio)
+    resized = im.resize((width, height), Image.ANTIALIAS)
+    image_buffer.seek(0)
+    target_file = os.path.join(dest_path, "{}{}".format(prefix, img))
+    if not os.path.exists(dest_path):
+        os.mkdir(dest_path)
+    resized.save(target_file, im.format)
