7f4a574fa58c5a11894a4293d89a35c94a0f0872
Arnaud Menut <agmenut@gmail.com>
2015-12-28 09:53:29 -0800

    Fixed ratio calculation and implement transforms for most EXIF orientation values.

--- haxorbb/utilities/utils.py
+++ haxorbb/utilities/utils.py
@@ -1,7 +1,6 @@
 # *-* coding: utf-8 *-*
 import string
 import os
-from io import BytesIO
 from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 from warnings import warn
@@ -20,6 +19,24 @@ class Utilities(object):
         return delimiter.join(slug)[:30]
 
 
+def exif_orientation_transform(source, orientation):
+    angle_values = {2: Image.FLIP_LEFT_RIGHT,
+                    3: 180,
+                    4: Image.FLIP_TOP_BOTTOM,
+                    5: 'transpose_1',
+                    6: 270,
+                    7: 'transpose_2',
+                    8: 90}
+    if orientation == 1:
+        return source
+    elif orientation == 3 or orientation == 6 or orientation == 8:
+        return source.rotate(angle_values[orientation])
+    elif orientation == 2 or orientation == 4:
+        return source.transpose(angle_values[orientation])
+    else:
+        return source
+
+
 def generate_thumbnail(source, dest_path, source_path=None, height=None, width=None, prefix='tn_', preserve_exif=False):
     if height is None and width is None:
         raise ValueError('Either height or width value required')
@@ -28,16 +45,23 @@ def generate_thumbnail(source, dest_path, source_path=None, height=None, width=N
     path, img = os.path.split(source)
     if source_path:
         path = source_path
-    image_buffer = BytesIO()
     im = Image.open(os.path.join(path, source))
-    ratio = float(im.size[0]) / float(im.size[1])
+    ratio = float(im.width) / float(im.height)
+
+    exif_data = {TAGS[k]: v for k, v in im._getexif().items() if k in TAGS}
+
+    if not os.path.exists(dest_path):
+        os.mkdir(dest_path)
+
     if width:
         height = int(width / ratio)
     elif width is None and height:
         width = int(height / ratio)
     resized = im.resize((width, height), Image.ANTIALIAS)
-    image_buffer.seek(0)
+    orientation = exif_data['Orientation']
+
+    if orientation:
+        resized = exif_orientation_transform(resized, orientation)
+
     target_file = os.path.join(dest_path, "{}{}".format(prefix, img))
-    if not os.path.exists(dest_path):
-        os.mkdir(dest_path)
     resized.save(target_file, im.format)
