6978fa4a4b68720190b01ef2fb2684e25e60bd4d
Arnaud Menut <agmenut@gmail.com>
2016-01-11 07:30:15 -0800

    Don't try to get EXIF data from not jpeg sources.

--- haxorbb/utilities/utils.py
+++ haxorbb/utilities/utils.py
@@ -48,21 +48,23 @@ def generate_thumbnail(source, dest_path, source_path=None, height=None, width=N
     im = Image.open(os.path.join(path, source))
     ratio = float(im.width) / float(im.height)
 
-    exif_data = {TAGS[k]: v for k, v in im._getexif().items() if k in TAGS}
+    try:
+        exif_data = {TAGS[k]: v for k, v in im._getexif().items() if k in TAGS}
+        orientation = exif_data['Orientation']
+    except AttributeError:
+        orientation = None
 
     if not os.path.exists(dest_path):
         os.mkdir(dest_path)
-
-    orientation = exif_data['Orientation']
-
-    if orientation:
-        im = exif_orientation_transform(im, orientation)
 
     if width:
         height = int(width / ratio)
     elif width is None and height:
         width = int(height / ratio)
     resized = im.resize((width, height), Image.ANTIALIAS)
+
+    if orientation:
+        resized = exif_orientation_transform(resized, orientation)
 
     target_file = os.path.join(dest_path, "{}{}".format(prefix, img))
     resized.save(target_file, im.format)
