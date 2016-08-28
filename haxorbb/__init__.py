e7d8425a8a343a30e7d11774575810eec578d78a
Arnaud Menut <agmenut@gmail.com>
2016-08-28 08:39:04 -0700

    Proper attribution for the ReverseProxied class.

--- haxorbb/__init__.py
+++ haxorbb/__init__.py
@@ -11,7 +11,11 @@ from logging import Formatter
 
 
 class ReverseProxied(object):
-    """Wrap the application in this middleware and configure the
+    """
+
+    From http://flask.pocoo.org/snippets/35/
+
+    Wrap the application in this middleware and configure the
     front-end server to add these headers, to let you quietly bind
     this to a URL other than / and to an HTTP scheme that is
     different than what is used locally.
